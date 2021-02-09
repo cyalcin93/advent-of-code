8709bc6c05849897b5b6c1d9c25e33a2eefe0206
cyalcin <cyalcin93@gmail.com>
2021-02-09 08:22:39 +0100

    Day7part1

---
+++ src/main/kotlin/Day7Part1.kt
@@ -0,0 +1,15 @@
+
+
+class Day7 {
+    fun part1(input: String): Int {
+        val rules = input.split("\n")
+        // Vi bryr oss inte om hur många shiny gold bags det finns
+        
+
+        return 4
+    }
+
+    private fun digUp(rule: String) {
+
+    }
+}
